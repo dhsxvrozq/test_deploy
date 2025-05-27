d5ff6269fbcab70fc47bc160d25d4d4ea4b89d2f
ivanpopov <ivanpopov140496@gmail.com>
2025-05-27 22:34:57 +0200

    2025-05-27 22:34:57

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
+# меняем имя хоста
+
 apt install python3-pip -y
 pip install aiofiles
 wget https://raw.githubusercontent.com/dhsxvrozq/test_deploy/refs/heads/master/start.py/change_hostname.py
 chmod +x change_hostname.py
 python3 change_hostname.py node3 127.0.1.1
+
