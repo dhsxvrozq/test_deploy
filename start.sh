dc449845f066ed6d50515a68a80ed9fb7aec83fa
ivanpopov <ivanpopov140496@gmail.com>
2025-05-30 12:13:35 +0200

    2025-05-30 12:13:35

--- start.sh
+++ start.sh
@@ -1,17 +1,12 @@
-# меняем имя хоста
-
+apt update
 wget https://raw.githubusercontent.com/dhsxvrozq/test_deploy/refs/heads/master/start.py/change_hostname.py
 chmod +x change_hostname.py
-
 wget https://raw.githubusercontent.com/dhsxvrozq/test_deploy/refs/heads/master/.env
 chmod 777 .env
 source .env
-
-
-apt install python3-pip -y
+export $(cat .env | xargs)
+apt install -y python3-pip 
 pip install aiofiles
 python3 change_hostname.py $SERVER_NAME 127.0.1.1
-
-
 wget https://raw.githubusercontent.com/dhsxvrozq/test_deploy/refs/heads/master/vless_manager.py
 chmod +x vless_manager.py
