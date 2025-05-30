882399c2c2b87a0c395ecaf3ed0e731d2a0172ba
ivanpopov <ivanpopov140496@gmail.com>
2025-05-30 11:37:33 +0200

    2025-05-30 11:37:33

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 wget https://raw.githubusercontent.com/dhsxvrozq/test_deploy/refs/heads/master/start.py/change_hostname.py
 chmod +x change_hostname.py
 
-wget 
+wget https://raw.githubusercontent.com/dhsxvrozq/test_deploy/refs/heads/master/.env
+chmod 400 .env
 source .env
 
 
@@ -12,4 +13,4 @@ pip install aiofiles
 python3 change_hostname.py $SERVER_NAME 127.0.1.1
 
 wget https://raw.githubusercontent.com/dhsxvrozq/test_deploy/refs/heads/master/vless_manager_swarm.py
-chmod +x vless_manager_swarm.py
+chmod +x vless_manager.py
