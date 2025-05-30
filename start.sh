3dcf73d971d8e3a2f94b34cfcc6476ff4a87e365
ivanpopov <ivanpopov140496@gmail.com>
2025-05-30 11:45:18 +0200

    2025-05-30 11:45:18

--- start.sh
+++ start.sh
@@ -12,5 +12,6 @@ apt install python3-pip -y
 pip install aiofiles
 python3 change_hostname.py $SERVER_NAME 127.0.1.1
 
-wget https://raw.githubusercontent.com/dhsxvrozq/test_deploy/refs/heads/master/vless_manager_swarm.py
+
+wget https://raw.githubusercontent.com/dhsxvrozq/test_deploy/refs/heads/master/vless_manager.py
 chmod +x vless_manager.py
